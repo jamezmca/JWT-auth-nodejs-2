01b169ef6b6091fca9c1e3e6f4eda348043b6ee3
James <jamesmca@live.com>
2021-05-30 14:05:57 -0700

    set up port

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const express = require('express')
 const app = express()
+
+//import routes
+const authRoute = require('./routes/auth')
+
+//reoute middlewares
+app.use('./api/user', authRoute)
+
+app.listen(3000, ()=> console.log('server up and running'))
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+
+router.post('/register', (req, res) => {
+    res.sendStatus('Register')
+})
+
+
+
+module.exports = router
